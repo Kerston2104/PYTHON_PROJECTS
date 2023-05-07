a83d4b71978ae296d163e02498de8b77b8f456f2
Kerston2104 <113289902+Kerston2104@users.noreply.github.com>
2023-05-07 16:15:11 +0530

    add files

--- exno2/4perfect_or_not.py
+++ exno2/4perfect_or_not.py
@@ -1,11 +1,14 @@
 # TO FIND THE GIVE NUMBER IS A PERFECT NUMBER
 
-inp=int(input("ENTER THE NUMBER:"))
-my_list=[]
-for i in range(1,inp):
-    if inp%1==0:
-        my_list.append(i)
-        if sum(my_list)==inp:
-            print(inp,"PERFECT NUMBER")
+def perf(inp):
+    my_list=[]
+    for i in range(1,inp):
+        if inp%i==0:
+            my_list.append(i)
+    if sum(my_list)==inp:
+        print(inp,"PERFECT NUMBER")
     else:
         print(inp,"IS NOT A PERFECT NUMBER")
+
+inpu=int(input("ENTER THE NUMBER:"))
+perf(inpu)
---
+++ exno2/5.upp_low_spa.py
@@ -0,0 +1,20 @@
+# TO FIND THE NO OF UPPER LOWER AND SPACE 
+
+def count(string):
+    up=0
+    lo=0
+    sp=0
+    for ele in string:
+        if ele.isupper():
+            up+=1
+        elif ele.islower():
+            lo+=1
+        elif ele==' ':
+            sp+=1
+    print(up,"NO OF UPPERCASE")
+    print(lo,"NO OF LOWER CASE")
+    print(sp,"NO OF BLANK SPACE")
+
+string=input("ENTER THE STRING:")
+count(string)
+
